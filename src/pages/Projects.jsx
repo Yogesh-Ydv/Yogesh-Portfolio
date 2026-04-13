77f9b4786eb2b40757e2591d532214e36121652e
Yogesh Yadav <yogeshydv247@gamil.com>
2026-04-14 02:25:13 +0530

    portfolio vercel live link

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -25,7 +25,7 @@ const Projects = () => {
       'A modern personal portfolio built in React with Tailwind CSS, showcasing dedicated pages for Home, About, Skills, Projects, and Contact, delivering a smooth and immersive browsing experience with animations and responsive design.',
     tech: ['React', 'Tailwind CSS', 'JavaScript'],
     github: 'https://github.com/Yogesh-Ydv/Yogesh-Portfolio',
-    live: ''
+    live: 'https://yogesh-portfolio-nine-beta.vercel.app/'
   },
   {
     title: 'Todo List React App',
